e5f1b0f23cfd06ad779dc4bb1b0c55301fe4bc20
Hafis Mohamed <hafispmh755@gmil.com>
2024-03-26 20:21:00 +0530

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route:: get('/user/{id}',[UserController::class, 'getUser']);
+
